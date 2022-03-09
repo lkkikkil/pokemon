a47f764454cd05d647ff16c4ff07e762af68351f
seong min Kim <skattr13@naver.com>
2022-03-09 21:48:56 +0900

    Make startMenuSelect menu

--- src/main/java/StartMenu.java
+++ src/main/java/StartMenu.java
@@ -1,3 +1,11 @@
-public class StartMenu {
+import java.util.Scanner;
 
+public class StartMenu {
+    public void startMenuSelect() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println( "1. 모험을 계속하다\n" +
+                            "2. 새로운 모험을 시작하다\n" +
+                            "3. 설정을 바꾸다\n");
+        int selectNum = scan.nextInt();
+    }
 }
