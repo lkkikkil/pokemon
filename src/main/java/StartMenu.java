180ebe70ba66f84e83d5d2823d9d44b5958441e9
seong min Kim <skattr13@naver.com>
2022-03-12 16:24:00 +0900

    Implement startMenuSelect

--- src/main/java/StartMenu.java
+++ src/main/java/StartMenu.java
@@ -1,11 +1,26 @@
 import java.util.Scanner;
 
 public class StartMenu {
+    Scanner scan = new Scanner(System.in);
+
     public void startMenuSelect() {
-        Scanner scan = new Scanner(System.in);
         System.out.println( "1. 모험을 계속하다\n" +
                             "2. 새로운 모험을 시작하다\n" +
                             "3. 설정을 바꾸다\n");
         int selectNum = scan.nextInt();
+
+        if(selectNum == 1) {
+            // gameContinue();
+        }
+        else if(selectNum == 2) {
+            // newGame();
+        }
+        else if(selectNum == 3) {
+            // menuSetting();
+        }
+        else {
+            System.out.println("다시 입력해주십시오.");
+            startMenuSelect();
+        }
     }
 }
