e4c05371c920685d9c1668de469a67f5edfc4f1b
seong min Kim <skattr13@naver.com>
2022-03-12 20:32:13 +0900

    Implement newGame function

--- src/main/java/StartMenu.java
+++ src/main/java/StartMenu.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class StartMenu {
@@ -8,12 +11,13 @@ public class StartMenu {
                             "2. 새로운 모험을 시작하다\n" +
                             "3. 설정을 바꾸다\n");
         int selectNum = scan.nextInt();
+        scan.nextLine();
 
         if(selectNum == 1) {
             // gameContinue();
         }
         else if(selectNum == 2) {
-            // newGame();
+            newGame();
         }
         else if(selectNum == 3) {
             // menuSetting();
@@ -21,6 +25,39 @@ public class StartMenu {
         else {
             System.out.println("다시 입력해주십시오.");
             startMenuSelect();
+        }
+    }
+
+    private void newGame() {
+        System.out.println("게임 진행 시에는 엔터(enter)로 대화를 넘겨주십시오");
+        scan.nextLine();
+
+        Scanner textScan = null;
+        try {
+            textScan = new Scanner(new File("./src/main/resources/newGameText.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (textScan.hasNextLine()) {
+            String text = textScan.nextLine();
+            System.out.println(text);
+            if(text.equals("그럼... 슬슬 너의 이름을 가르쳐다오")) {
+                String userName = null;
+                int nameCheck = 0;
+                do {
+                    System.out.println("이름을 입력하세요:");
+                    userName = scan.next();
+                    System.out.println("자네의 이름이" + userName + "이(가) 맞는가?\n" +
+                            "1. 맞습니다. 2. 아닙니다.");
+                    nameCheck = scan.nextInt();
+                    while (!(nameCheck == 1 || nameCheck == 2)) {
+                        System.out.println("1 혹은 2로 다시 입력해주세요");
+                        nameCheck = scan.nextInt();
+                    }
+                } while (nameCheck == 2);
+            }
+            scan.nextLine();
         }
     }
 }
